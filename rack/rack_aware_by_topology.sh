886703daa61390526e6b36924f8f84f04094fe58
David W. Streever <david@streever.com>
2014-05-28 12:06:08 -0400

    rack scripts

---
+++ rack/rack_aware_by_topology.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Adjust/Add the property "topology.script.file.name"
+# to core-site.xml with the "absolute" path the this
+# file.  ENSURE the file is "executable".
+
+# Supply appropriate rack prefix
+RACK_PREFIX=default
+
+# To test, supply a hostname as script input:
+if [ $# -gt 0 ]; then
+
+CTL_FILE=${CTL_FILE:-"rack_topology.data"}
+
+HADOOP_CONF=${HADOOP_CONF:-"/etc/hadoop/conf"} 
+
+if [ ! -f ${HADOOP_CONF}/${CTL_FILE} ]; then
+  echo -n "/$RACK_PREFIX/rack "
+  exit 0
+fi
+
+while [ $# -gt 0 ] ; do
+  nodeArg=$1
+  exec< ${HADOOP_CONF}/${CTL_FILE}
+  result=""
+  while read line ; do
+    ar=( $line )
+    if [ "${ar[0]}" = "$nodeArg" ] ; then
+      result="${ar[1]}"
+    fi
+  done
+  shift
+  if [ -z "$result" ] ; then
+    echo -n "/$RACK_PREFIX/rack "
+  else
+    echo -n "/$RACK_PREFIX/rack_$result "
+  fi
+done
+
+else
+  echo -n "/$RACK_PREFIX/rack "
+fi
