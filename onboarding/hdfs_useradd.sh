9725613c2810f20d98b0419c5cfcae5cffde00f0
David W. Streever <david@streever.com>
2015-07-01 16:44:03 -0400

    added private.db

--- onboarding/hdfs_useradd.sh
+++ onboarding/hdfs_useradd.sh
@@ -2,6 +2,9 @@
 
 HDFS_USER=$1
 
-hdfs dfs -mkdir /user/${HDFS_USER}
+hdfs dfs -mkdir /user/${HDFS_USER}/private.db
 hdfs dfs -chown -R ${HDFS_USER} /user/${HDFS_USER}
 
+echo "You need to give 'hive' rwx permissions to /user/${HDFS_USER}/private.db"
+echo ""
+echo "That can be done through Ranger HDFS Repo OR through Extended HDFS ACL's"
